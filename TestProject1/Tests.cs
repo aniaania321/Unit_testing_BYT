cc9dcd1264e17dd2881205d55bfdd193fc7f7dbe
aniaania321 <s31154@pjwstk.edu.pl>
2025-11-07 12:35:53 +0100

    Created tests for remaining shape classes (task 1)

--- TestProject1/Tests.cs
+++ TestProject1/Tests.cs
@@ -18,5 +18,46 @@ public class Tests
         Assert.That(sphere.CalculateVolume(), Is.EqualTo(523.598).Within(0.001));
     }
 
-    //todo : Complete the remaning tests here
+    private readonly IShape cylinder = new Cylinder(3, 7);
+    
+    [Test]
+    public void TestCylinderCalculateArea()
+    {
+        Assert.That(cylinder.CalculateArea(), Is.EqualTo(188.496).Within(0.001));
+    }
+
+    [Test]
+    public void TestCylinderCalculateVolume()
+    {
+        Assert.That(cylinder.CalculateVolume(), Is.EqualTo(197.920).Within(0.001));
+    }
+    
+    private readonly IShape rectangle = new Rectangle(4, 8);
+    
+    [Test]
+    public void TestRectangleCalculateArea()
+    {
+        Assert.That(rectangle.CalculateArea(), Is.EqualTo(32.000).Within(0.001));
+    }
+
+    [Test]
+    public void TestRectangleCalculateVolume()
+    {
+        Assert.That(rectangle.CalculateVolume(), Is.EqualTo(0.000).Within(0.001));
+    }
+    
+    private readonly IShape cube = new Cube(4);
+    
+    [Test]
+    public void TestCubeCalculateArea()
+    {
+        Assert.That(cube.CalculateArea(), Is.EqualTo(96.000).Within(0.001));
+    }
+
+    [Test]
+    public void TestCubeCalculateVolume()
+    {
+        Assert.That(cube.CalculateVolume(), Is.EqualTo(64.000).Within(0.001));
+    }
+    
 }
