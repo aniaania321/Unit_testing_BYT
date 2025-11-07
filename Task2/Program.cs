b0b9515fd3c72b300b8bb6008fd3988b354be84a
aniaania321 <s31154@pjwstk.edu.pl>
2025-11-07 17:17:02 +0100

    Added Program.cs because it was not in git

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿using Task2;
+
+Calculator calculator1 = new Calculator(10,2,"/");
+Console.WriteLine(calculator1.Calculate());
+
+Calculator calculator2 = new Calculator(10,5,"*");
+Console.WriteLine(calculator2.Calculate());
+
+Calculator calculator4 = new Calculator(8,12,"+");
+Console.WriteLine(calculator4.Calculate());
+
+Calculator calculator3 = new Calculator(10,8,"-");
+Console.WriteLine(calculator3.Calculate());
